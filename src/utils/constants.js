dd2903bb27d08ec037af0065652a8e4c53ae7331
Alrian Yaseen <alrianvoice1@gmail.com>
2023-04-25 18:37:57 +0500

    Added utils

---
+++ src/utils/constants.js
@@ -0,0 +1,18 @@
+export const stepsToPlay = [
+  { stepNumber: 1, stepText: 'Click on "Play"' },
+  { stepNumber: 2, stepText: "Read the question and choose an answer" },
+  {
+    stepNumber: 3,
+    stepText: "Click on the radio button next to the answer you have selected",
+  },
+  { stepNumber: 4, stepText: 'Click on "Submit"' },
+  {
+    stepNumber: 5,
+    stepText:
+      "If correct, proceed to the next question, otherwise try again or move on",
+  },
+  {
+    stepNumber: 6,
+    stepText: "The quiz continues with new questions until the end",
+  },
+];
