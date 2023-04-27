ea8a54fd5f556d6578841dd651c0115943b9c5e9
Alrian Yaseen <alrianvoice1@gmail.com>
2023-04-27 13:21:54 +0500

    Added the categories to the constants and made the select tags functioning using useState and onChange functions

--- src/components/SetupQuiz.jsx
+++ src/components/SetupQuiz.jsx
@@ -1,6 +1,10 @@
-import React from "react";
-
+import React, { useState } from "react";
+import { categories } from "../utils/constants";
 function SetupQuiz() {
+  const [category, setCategory] = useState("History");
+  const [difficulty, setDifficulty] = useState("easy");
+
+  console.log(difficulty);
   return (
     <div className="flex items-center justify-center font-sans h-screen bg-slate-200">
       <div className="bg-white shadow-xl h-[500px] rounded-lg w-[430px] p-6">
@@ -14,33 +18,30 @@ function SetupQuiz() {
           </div>
           <div className="text-gray-700 mb-8">
             <p className="mb-2">Select Category</p>
-            <select className="bg-gray-200 py-2 text-sm px-1 rounded-lg w-full font-semibold">
-             <option value="History">History</option>
-             <option value="Geography">Geography</option>
-             <option value="Science and Nature">Science and Nature</option>
-             <option value="Literature">Literature</option>
-             <option value="Arts and Entertainment">Arts and Entertainment</option>
-             <option value="Sports and Games">Sports and Games</option>
-             <option value="Current Affairs and Politics">Current Affairs and Politics</option>
-             <option value="Technology and Innovation">Technology and Innovation</option>
-             <option value="Food and Drink">Food and Drink</option>
-             <option value="Mythology and Folklore">Mythology and Folklore</option>
-             <option value="Language and Linguistics">Language and Linguistics</option>
-             <option value="Religion and Philosophy">Religion and Philosophy</option>
-             <option value="Famous People and Personalities">Famous People and Personalities</option>
+            <select
+              onChange={(e) => {
+                setCategory(e.target.value);
+              }}
+              className="bg-gray-200 py-2 text-sm px-1 rounded-lg w-full font-semibold"
+            >
+              {categories.map((category, index) => (
+                <option key={index} value={category}>
+                  {category}
+                </option>
+              ))}
             </select>
           </div>
           <div className="text-gray-700 mb-10">
             <p className="mb-2">Select Difficulty</p>
-            <select className="bg-gray-200 w-full py-2 text-sm px-1 rounded-lg font-semibold">
-              <option value="easy" >Easy</option>
-              <option value="medium" >Medium</option>
-              <option value="hard" >Hard</option>
+            <select onChange={(e)=>{setDifficulty(e.target.value)}} className="bg-gray-200 w-full py-2 text-sm px-1 rounded-lg font-semibold">
+              <option value="easy">Easy</option>
+              <option value="medium">Medium</option>
+              <option value="hard">Hard</option>
             </select>
           </div>
         </div>
         <button className="bg-yellow-400 font-bold px-3 py-2 rounded-md w-full text-center hover:scale-105 hover:shadow-md hover:bg-yellow-300 transition-transform ease-in-out ">
-            Start
+          Start
         </button>
       </div>
     </div>
--- src/utils/constants.js
+++ src/utils/constants.js
@@ -16,3 +16,17 @@ export const stepsToPlay = [
     stepText: "The quiz continues with new questions until the end",
   },
 ];
+
+export const categories = [
+  "History",
+  "Geography",
+  "Science and Nature",
+  "Literature",
+  "Arts and Entertainment",
+  "Current Affairs and Politics",
+  "Technology and Innovation",
+  "Mythology and Folklore",
+  "Language and Linguistics",
+  "Religion and Philosophy",
+  "Famous People and Personalities",
+];
